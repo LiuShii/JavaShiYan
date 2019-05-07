c1d60026718a87076bf740eea66759285d4a01c2
LiuShii <1164818081@qq.com>
2019-05-07 22:57:38 +0800

    1_1

--- src/com/company/changyonglei1_1.java
+++ src/com/company/changyonglei1_1.java
@@ -1,6 +1,19 @@
 package com.company;
 
 public class changyonglei1_1 {
-
+    int jisu(String str){
+        char[] ch=str.toCharArray();
+        int count=0;
+        for(int i=0;i<str.length()-1;i++){
+            if(ch[i]=='s')
+                count++;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        changyonglei1_1 cyl=new changyonglei1_1();
+        String s="this is a test of java";
+        System.out.println(" 1"+cyl.jisu(s));
+    }
 
 }

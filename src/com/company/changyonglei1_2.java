8e09c1356914d112b60ea9e4b016f3c0abe82402
LiuShii <1164818081@qq.com>
2019-05-07 23:02:51 +0800

    1_1

---
+++ src/com/company/changyonglei1_2.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class changyonglei1_2 {
+    String SubString(String str){
+        String s1=str.substring(10,14);
+        return s1;
+    }
+    public static void main(String[] args) {
+        changyonglei1_2 cyl =new changyonglei1_2();
+        String s="this is a test of java";
+        System.out.println(cyl.SubString(s));
+    }
+}

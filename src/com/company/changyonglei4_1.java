5045c8a916fc3399190a9ece5f12fdda9e7e8067
LiuShii <1164818081@qq.com>
2019-05-08 15:15:43 +0800

    4_1

--- src/com/company/changyonglei4_1.java
+++ src/com/company/changyonglei4_1.java
@@ -1,4 +1,22 @@
 package com.company;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class changyonglei4_1 {
+    boolean demo(String str){
+        String s = "^\\(\\d{4}\\)\\d{4}-\\d{4}$";
+        Pattern pattern =Pattern.compile(s);
+        Matcher matcher = pattern.matcher(str);
+        return matcher.matches();
+    }
+
+    public static void main(String[] args) {
+        String s1= "(0000)0000-0000";
+        String s2 = "2222)0000-1111";
+
+        changyonglei4_1 cyl = new changyonglei4_1();
+        System.out.println(cyl.demo(s1));
+        System.out.println(cyl.demo(s2));
+    }
 }

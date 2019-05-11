6dcfec973a568674ebda477fd1a9d6700d36e7de
LiuShii <1164818081@qq.com>
2019-05-11 18:53:53 +0800

    4_3

---
+++ src/com/company/changyonglei4_3.java
@@ -0,0 +1,19 @@
+package com.company;
+
+
+public class changyonglei4_3 {
+    String RpDisappear(String str){
+
+        String s =str.replaceAll("(\\w)/1*","");
+        return s;
+
+
+    }
+
+    public static void main(String[] args) {
+
+        changyonglei4_3 cyl = new changyonglei4_3();
+        String s = "aaa";
+        System.out.println(cyl.RpDisappear(s));
+    }
+}
